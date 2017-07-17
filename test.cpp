3e0ab8962570009b219ae2b8bd4458fb25973bb1
Your Name <you@example.com>
2017-07-18 01:36:01 +0800

    Add the initial version of cstyle-check

---
+++ test.cpp
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+class AX{
+	
+	int a;
+};
+
+
+int main(int argc, char *argv) {
+    int a_aaaaa;
+
+    int tableAAA;
+    int table_aa;
+
+    string tableName;
+    return 0;
+}
---
+++ webkitpy/style/test.cpp
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+class AX{
+	
+	int a;
+};
+
+
+int main(int argc, char *argv) {
+    int Aa;
+    string tableName;
+    return 0;
+}
